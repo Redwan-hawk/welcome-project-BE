dbbddf7a8cad057cab5c9d01a92df997ecf4b4be
LAU CHUN YI <CHUNYI.24@ichat.sp.edu.sg>
2026-04-07 16:26:51 +0800

    backend/user
    Create backend for get user data by id, update user and change password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,3 +11,61 @@ exports.getAllUsers = async (req, res) => {
     console.log(`[${new Date().toISOString()}] GET /api/users - Completed`);
   }   
 };
+
+exports.getUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const result = await userModel.getUserById(userId);
+    if (result.length === 0) {
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      res.status(200).json(result[0]);
+    }
+  } catch (err) {
+    console.error('Error fetching user by ID:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    console.log(`[${new Date().toISOString()}] GET /api/users/${req.params.id} - Completed`);
+  }
+};
+
+exports.updateUserInfo = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const { user_name, user_email } = req.body;
+    const result = await userModel.updateUserInfo(userId, user_name, user_email);
+    if (result.rowCount === 0) {
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      res.status(200).json({ message: 'User updated successfully' });
+    } 
+  } catch (err) {
+    console.error('Error updating user:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    console.log(`[${new Date().toISOString()}] PUT /api/users/${req.params.id} - Completed`);
+  }
+};
+
+exports.updateUserPassword = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    // Get both passwords from the request body
+    const { currentPassword, newPassword } = req.body;
+
+    const result = await userModel.updateUserPassword(userId, currentPassword, newPassword);
+
+    if (result.status === 404) {
+      return res.status(404).json({ error: 'User not found' });
+    } 
+    
+    if (result.status === 401) {
+      return res.status(401).json({ error: 'Current password is incorrect' });
+    }
+
+    res.status(200).json({ message: 'Password updated successfully' });
+  } catch (err) {
+    console.error('Error updating user password:', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/", userController.getAllUsers);
+router.get("/:id", userController.getUserById);
+router.put("/:id", userController.updateUserInfo);
+router.put("/:id/password", userController.updateUserPassword);
 
 module.exports = router;
