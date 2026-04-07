0d1b0c76f29c29a33405f71cdb100b94f8e29d98
junming031206 <junming03.24@ichat.sp.edu.sg>
2026-04-07 17:09:07 +0800

    bugfix for inventory endpoint

--- src/routes/inventoryRoute.js
+++ src/routes/inventoryRoute.js
@@ -4,6 +4,5 @@ const inventoryController = require("../controllers/inventoryController");
 
 router.get("/", inventoryController.getAllInventoryItems);
 router.get('/low-stock', inventoryController.getLowStockItems);
-router.get('/:id', inventoryController.getInventoryById);
 
 module.exports = router;
