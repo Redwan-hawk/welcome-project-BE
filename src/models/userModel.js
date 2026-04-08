c3ffb8e70ae4f90d18927b3d37835b9f5df58a90
LAU CHUN YI <CHUNYI.24@ichat.sp.edu.sg>
2026-04-08 10:36:35 +0800

    Profile updated date and fix small bug
    Edit the userModel to make sure the update_at will updated when the user update profile info and change password. Fix small bug in route

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -23,7 +23,7 @@ exports.getUserById = async (userId) => {
 exports.updateUserInfo = async (userId, name, email) => {
     try {
         const res = await pool.query(
-            'UPDATE users SET username = $1, user_email = $2 WHERE user_id = $3',
+            'UPDATE users SET username = $1, user_email = $2, updated_at = NOW() WHERE user_id = $3',
             [name, email, userId]
         );
         return res;
@@ -56,7 +56,7 @@ exports.updateUserPassword = async (userId, currentPassword, newPassword) => {
 
     // 3. Update to the new password
     await pool.query(
-      'UPDATE users SET user_password = $1 WHERE user_id = $2',
+      'UPDATE users SET user_password = $1, updated_at = NOW() WHERE user_id = $2',
       [newPassword, userId]
     );
 
